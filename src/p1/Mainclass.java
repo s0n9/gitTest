81de4efb63537cbe6492b458472c836f5729b36f
s0n9 <1098781982@qq.com>
2018-11-01 16:49:52 +0800

    eclipse提交

---
+++ src/p1/Mainclass.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=10){
+			System.out.println("q");
+			i++;
+		}
+		System.out.println("a");
+		
+	}
+	
+}
